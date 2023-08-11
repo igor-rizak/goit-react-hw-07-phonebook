5e13c846b51dabaaff6b356615b60c16b342c2a6
igor-rizak <igor.rizak@ukr.net>
2023-08-11 10:15:50 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -55,7 +55,6 @@ const ContactForm = () => {
           type="text"
           name="name"
           onChange={handleChange}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           value={name}
@@ -69,7 +68,6 @@ const ContactForm = () => {
           name="number"
           value={number}
           onChange={handleChange}
-          pattern="\+?\d{1,4}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
         />
